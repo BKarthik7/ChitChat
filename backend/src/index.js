9d5d2ba55d8767814127a9bcd8974a72413bbfde
B Karthik <bangikarthik7@gmail.com>
2025-03-06 20:47:04 +0530

    cookieParser issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.route.js';
-import {connectDB} from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
@@ -11,10 +12,11 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
-  connectDB()
+	console.log("Server is running on port", PORT);
+	connectDB();
 });
-
