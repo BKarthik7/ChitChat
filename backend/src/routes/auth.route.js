12431d50d8661e3ba4aa7446a3db3d9b8227f9b7
B Karthik <bangikarthik7@gmail.com>
2025-03-06 20:52:36 +0530

    checkAuth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,7 @@ import {
 	logout,
 	signup,
 	updateProfile,
+	checkAuth,
 } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js";
 
@@ -17,4 +18,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check", protectRoute, checkAuth)
+
 export default router;
